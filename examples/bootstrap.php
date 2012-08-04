1dc9e1926ee9bf063876cda6251db3d42f37f5cf
PeeHaa <info@pieterhordijk.com>
2012-08-04 16:28:15 +0200

    Added examples bootstrap file

---
+++ examples/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bootstrap the library
+ */
+require_once '/../OAuth/bootstrap.php';
+
+/**
+ * Setup error reporting
+ */
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+/**
+ * Setup the timezone
+ */
+ini_set('date.timezone', 'Europe/Amsterdam');

92cdb8e235462b9eb25dc53b73cec60059b5caa1
PeeHaa <info@pieterhordijk.com>
2012-08-04 18:20:49 +0200

    Added instance of the URI class with the current URI

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Bootstrap file for the different examples. This file should be included at the top of al examples to easiliy load
+ * the library files and setup the defaults for the examples.
+ *
+ * PHP version 5.4
+ *
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 The authors
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
 
 /**
  * Bootstrap the library
@@ -15,3 +25,14 @@ ini_set('display_errors', 1);
  * Setup the timezone
  */
 ini_set('date.timezone', 'Europe/Amsterdam');
+
+/**
+ * Create a new instance of the URI class with the current URI
+ */
+$currentUri = new OAuth\Common\Http\Uri(
+    $_SERVER['SERVER_NAME'],
+    $_SERVER['REQUEST_URI'],
+    $_SERVER['SERVER_PROTOCOL'],
+    $_SERVER['SERVER_PORT'],
+    empty($_SERVER['HTTPS']) ? null : $_SERVER['HTTPS']
+);

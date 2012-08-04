1f60924ffd298aceb9011fe62b3a183a23a0c07d
PeeHaa <info@pieterhordijk.com>
2012-08-04 18:23:18 +0200

    Cleaned up Google service example

--- examples/google.php
+++ examples/google.php
@@ -1,29 +1,25 @@
 <?php
 /**
- * @author Lusitanian <alusitanian@gmail.com>
- * @author Pieter Hordijk <info@pieterhordijk.com>
- * Released under the MIT license.
+ * Example of retrieving an authentication token of the Google service
+ *
+ * PHP version 5.4
+ *
+ * @author     Lusitanian <alusitanian@gmail.com>
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 The authors
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  */
-
 use OAuth\OAuth2\Service\Google;
 use OAuth\Common\Storage\Null;
 use OAuth\Common\Consumer\Credentials;
 
+/**
+ * Bootstrap the example
+ */
 require_once __DIR__ . '/bootstrap.php';
 
-// public domain code that i'm using because of how lazy i was when trying to test this
-function get_own_url()
-{
-    $s = empty($_SERVER['HTTPS']) ? '' : ($_SERVER['HTTPS'] == 'on') ? 's' : '';
-    $protocol = strleft(strtolower($_SERVER["SERVER_PROTOCOL"]), '/').$s;
-    $port = ( $_SERVER['SERVER_PORT'] == '80' || $_SERVER['SERVER_PORT'] == '443' ) ? '' : (':' . $_SERVER['SERVER_PORT'] );
-    return $protocol . '://' . $_SERVER['SERVER_NAME'] . $port . $_SERVER['SCRIPT_NAME'];
-}
-
-function strleft($s1, $s2) { return substr($s1, 0, strpos($s1, $s2)); }
-
 $storage = new Null();
-$credentials = new Credentials('xxx', 'xxx', get_own_url() );
+$credentials = new Credentials('xxx', 'xxx', $currentUri->getAbsoluteUri());
 $httpClient = new OAuth\Common\Http\StreamClient();
 $googleService = new Google($credentials, $httpClient, $storage, [ Google::SCOPE_EMAIL, Google::SCOPE_PROFILE ]);
 
@@ -35,6 +31,6 @@ if( !empty( $_GET['code'] ) ) {
     $url = $googleService->getAuthorizationUrl();
     header('Location: ' . $url);
 } else {
-    $url = get_own_url() . '?go=go';
+    $url = $currentUri->getRelativeUri() . '?go=go';
     echo "<a href='$url'>Login with Google!</a>";
 }

05cf95187c0f11dc2494703e0374775d5fa56438
PeeHaa <info@pieterhordijk.com>
2012-08-05 23:31:20 +0200

    Added file docblocks

--- lib/OAuth/OAuth2/Service/Google.php
+++ lib/OAuth/OAuth2/Service/Google.php
@@ -1,16 +1,31 @@
 <?php
 /**
- * @author Lusitanian <alusitanian@gmail.com>
- * Released under the MIT license.
+ * OAuth2 service implementation for Google.
+ *
+ * PHP version 5.4
+ *
+ * @category   OAuth
+ * @package    OAuth2
+ * @subpackage Service
+ * @author     Lusitanian <alusitanian@gmail.com>
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 The authors
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  */
-
 namespace OAuth\OAuth2\Service;
+
 use OAuth\OAuth2\Token\StdOAuth2Token;
 use OAuth\Common\Http\Exception\TokenResponseException;
 use OAuth\Common\Http\Uri;
 
 /**
- * OAuth2 service implementation for Google
+ * OAuth2 service implementation for Google.
+ *
+ * @category   OAuth
+ * @package    OAuth2
+ * @subpackage Service
+ * @author     Lusitanian <alusitanian@gmail.com>
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
  */
 class Google extends AbstractService
 {

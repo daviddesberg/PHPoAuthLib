d96cae668fff19c674a65d63fe3853f3190ad0f3
David Desberg <david@thedesbergs.com>
2012-08-08 13:10:55 -0400

    Made Uri mutable

--- tests/OAuth/Unit/Common/Http/UriTest.php
+++ tests/OAuth/Unit/Common/Http/UriTest.php
@@ -16,4 +16,33 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->assertEquals( 'div', $uri->getFragment() );
         $this->assertEquals( 'joe:********', $uri->getUserInfo() );
     }
+
+    public function testFromParts()
+    {
+        $absoluteUri = 'https://joe:bob@example.com:6912/relative/path?user=jon&password=secret#div';
+        $uri = Uri::fromParts('https', 'joe:bob', 'example.com', 6912, '/relative/path', 'user=jon&password=secret', 'div');
+        $this->assertEquals( $absoluteUri, $uri->getAbsoluteUri() );
+        $this->assertEquals( 6912, $uri->getPort() );
+        $this->assertEquals( 'example.com', $uri->getHost() );
+        $this->assertEquals( 'https', $uri->getScheme() );
+        $this->assertEquals( '/relative/path', $uri->getPath() );
+        $this->assertEquals( 'joe:bob', $uri->getRawUserInfo() );
+        $this->assertEquals( 'div', $uri->getFragment() );
+        $this->assertEquals( 'joe:********', $uri->getUserInfo() );
+    }
+
+    public function testMutability()
+    {
+        $absoluteUri = 'https://joe:bob@example.com:6912/relative/path?user=jon&password=secret#div';
+        $uri = new Uri($absoluteUri);
+
+        $uri->setPort(443);
+        $this->assertEquals( str_replace(':6912', '', $absoluteUri ), $uri->getAbsoluteUri() );
+
+        $uri->setPort(6912);
+        $this->assertEquals( $absoluteUri, $uri->getAbsoluteUri() );
+
+        $uri->setHost('github.com');
+        $this->assertEquals( str_replace('example.com', 'github.com', $absoluteUri ), $uri->getAbsoluteUri() );
+    }
 }

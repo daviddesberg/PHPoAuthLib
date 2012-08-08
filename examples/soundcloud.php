9066bfcf91bf16eb13bab886eec8d7f3eb7e3273
PeeHaa <info@pieterhordijk.com>
2012-08-09 00:56:50 +0200

    Added SoundCloud service

---
+++ examples/soundcloud.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Example of retrieving an authentication token of the SoundCloud service
+ *
+ * PHP version 5.4
+ *
+ * @author     Lusitanian <alusitanian@gmail.com>
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 The authors
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+use OAuth\OAuth2\Service\SoundCloud;
+use OAuth\Common\Storage\Memory;
+use OAuth\Common\Consumer\Credentials;
+use OAuth\Common\Http\Uri;
+
+/**
+ * Bootstrap the example
+ */
+require_once __DIR__ . '/bootstrap.php';
+
+// In-memory storage
+$storage = new Memory();
+
+// Setup the credentials for the requests
+$credentials = new Credentials(
+    $servicesCredentials['soundcloud']['key'],
+    $servicesCredentials['soundcloud']['secret'],
+    $currentUri->getAbsoluteUri()
+);
+
+// Instantiate the SoundCloud service using the credentials, http client and storage mechanism for the token
+$soundcloudService = new SoundCloud($credentials, $httpClientProvider(), $storage, [ ]);
+
+if( !empty( $_GET['code'] ) ) {
+    // This was a callback request from SoundCloud, get the token
+    $soundcloudService->requestAccessToken( $_GET['code'] );
+
+    // Send a request with it
+    $result = json_decode( $soundcloudService->sendAuthenticatedRequest( new Uri('https://api.soundcloud.com/me.json'), [], 'GET' ), true );
+
+    // Show some of the resultant data
+    echo 'Your unique user id is: ' . $result['id'] . ' and your name is ' . $result['username'];
+
+} elseif( !empty($_GET['go'] ) && $_GET['go'] == 'go' ) {
+    $url = $soundcloudService->getAuthorizationUri();
+    header('Location: ' . $url);
+} else {
+    $url = $currentUri->getRelativeUri() . '?go=go';
+    echo "<a href='$url'>Login with SoundCloud!</a>";
+}
---
+++ src/OAuth/OAuth2/Service/SoundCloud.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * OAuth2 service implementation for SoundCloud.
+ *
+ * PHP version 5.4
+ *
+ * @category   OAuth
+ * @package    OAuth2
+ * @subpackage Service
+ * @author     Lusitanian <alusitanian@gmail.com>
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 The authors
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+namespace OAuth\OAuth2\Service;
+
+use OAuth\OAuth2\Token\StdOAuth2Token;
+use OAuth\Common\Http\Exception\TokenResponseException;
+use OAuth\Common\Http\Uri;
+
+/**
+ * OAuth2 service implementation for SoundCloud.
+ *
+ * @category   OAuth
+ * @package    OAuth2
+ * @subpackage Service
+ * @author     Lusitanian <alusitanian@gmail.com>
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class SoundCloud extends AbstractService
+{
+    /**
+     * @return \OAuth\Common\Http\Uri|\OAuth\Common\Http\UriInterface
+     */
+    public function getAuthorizationEndpoint()
+    {
+        return new Uri('https://soundcloud.com/connect');
+    }
+
+    /**
+     * @return \OAuth\Common\Http\Uri|\OAuth\Common\Http\UriInterface
+     */
+    public function getAccessTokenEndpoint()
+    {
+        return new Uri('https://api.soundcloud.com/oauth2/token');
+    }
+
+    /**
+     * @param string $responseBody
+     * @return \OAuth\Common\Token\TokenInterface|\OAuth\OAuth2\Token\StdOAuth2Token
+     * @throws \OAuth\Common\Http\Exception\TokenResponseException
+     */
+    protected function parseAccessTokenResponse($responseBody)
+    {
+        $data = json_decode( $responseBody, true );
+
+        if( null === $data || !is_array($data) ) {
+            throw new TokenResponseException('Unable to parse response.');
+        } elseif( isset($data['error'] ) ) {
+            throw new TokenResponseException('Error in retrieving token: "' . $data['error'] . '"');
+        }
+
+        $token = new StdOAuth2Token();
+        $token->setAccessToken( $data['access_token'] );
+        $token->setLifetime( $data['expires_in'] );
+
+        if( isset($data['refresh_token'] ) ) {
+            $token->setRefreshToken( $data['refresh_token'] );
+            unset($data['refresh_token']);
+        }
+
+        unset( $data['access_token'] );
+        unset( $data['expires_in'] );
+
+        $token->setExtraParams( $data );
+
+        return $token;
+    }
+}

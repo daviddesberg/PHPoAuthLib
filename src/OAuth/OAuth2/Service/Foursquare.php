c38b1d05f256b135643777a94699d7b86447ac30
PeeHaa <info@pieterhordijk.com>
2012-08-09 22:13:42 +0200

    Added Foursquare service

---
+++ src/OAuth/OAuth2/Service/Foursquare.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * OAuth2 service implementation for Foursquare.
+ *
+ * PHP version 5.4
+ *
+ * @category   OAuth
+ * @package    OAuth2
+ * @subpackage Service
+ * @author     Lusitanian <alusitanian@gmail.com>
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 The authors
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+namespace OAuth\OAuth2\Service;
+
+use OAuth\OAuth2\Token\StdOAuth2Token;
+use OAuth\Common\Http\Exception\TokenResponseException;
+use OAuth\Common\Http\Uri;
+
+/**
+ * OAuth2 service implementation for Foursquare.
+ *
+ * @category   OAuth
+ * @package    OAuth2
+ * @subpackage Service
+ * @author     Lusitanian <alusitanian@gmail.com>
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class Foursquare extends AbstractService
+{
+    /**
+     * @return \OAuth\Common\Http\Uri|\OAuth\Common\Http\UriInterface
+     */
+    public function getAuthorizationEndpoint()
+    {
+        return new Uri('https://foursquare.com/oauth2/authorize');
+    }
+
+    /**
+     * @return \OAuth\Common\Http\Uri|\OAuth\Common\Http\UriInterface
+     */
+    public function getAccessTokenEndpoint()
+    {
+        return new Uri('https://foursquare.com/oauth2/access_token');
+    }
+
+    /**
+     * @param string $responseBody
+     * @return \OAuth\Common\Token\TokenInterface|\OAuth\OAuth2\Token\StdOAuth2Token
+     * @throws \OAuth\Common\Http\Exception\TokenResponseException
+     */
+    protected function parseAccessTokenResponse($responseBody)
+    {
+        $data = json_decode( $responseBody, true );
+
+        if( null === $data || !is_array($data) ) {
+            throw new TokenResponseException('Unable to parse response.');
+        } elseif( isset($data['error'] ) ) {
+            throw new TokenResponseException('Error in retrieving token: "' . $data['error'] . '"');
+        }
+
+        $token = new StdOAuth2Token();
+
+
+        $token->setAccessToken( $data['access_token'] );
+        // Github tokens evidently never expire...
+        $token->setEndOfLife(StdOAuth2Token::EOL_NEVER_EXPIRES);
+        unset( $data['access_token'] );
+        $token->setExtraParams( $data );
+
+        return $token;
+    }
+}

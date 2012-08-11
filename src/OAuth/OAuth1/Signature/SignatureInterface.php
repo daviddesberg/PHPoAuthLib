b6301c2d60338981d8ce15a9782b28107917a858
PeeHaa <info@pieterhordijk.com>
2012-08-11 02:42:18 +0200

    Added signature interface

---
+++ src/OAuth/OAuth1/Signature/SignatureInterface.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Builds a signature to sign OAuth1 requests
+ *
+ * PHP version 5.4
+ *
+ * @author     Lusitanian <alusitanian@gmail.com>
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 The authors
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+namespace OAuth\OAuth1\Signature;
+
+use OAuth\Common\Consumer\Credentials;
+use OAuth\Common\Http\UriInterface;
+
+interface SignatureInterface
+{
+    /**
+     * @param \OAuth\Common\Consumer\Credentials $credentials
+     * @abstract
+     */
+    public function __construct(Credentials $credentials);
+
+    /**
+     * @param string $algorithm
+     * @abstract
+     */
+    public function setHashingAlgorithm($algorithm);
+
+    /**
+     * @param string $token
+     * @abstract
+     */
+    public function setTokenSecret($token);
+
+    /**
+     * @param \OAuth\Common\Http\UriInterface $uri
+     * @param mixed $requestBody
+     * @param array $authorizationHeader
+     * @param string $method
+     * @abstract
+     */
+    public function getSignature(UriInterface $uri, $requestBody = null, array $authorizationHeader = [], $method = 'POST');
+}

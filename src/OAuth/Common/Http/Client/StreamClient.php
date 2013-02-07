c9216eb832c0d19abf8ad119d324d16b26e854f2
Igor Wiedler <igor@wiedler.ch>
2013-02-08 00:39:31 +0100

    Fix typo & => &&

--- src/OAuth/Common/Http/Client/StreamClient.php
+++ src/OAuth/Common/Http/Client/StreamClient.php
@@ -61,7 +61,7 @@ class StreamClient implements ClientInterface
             throw new \InvalidArgumentException('No body expected for "GET" request.');
         }
 
-        if( !isset($extraHeaders['Content-type'] ) && $method === 'POST' & is_array($requestBody) ) {
+        if( !isset($extraHeaders['Content-type'] ) && $method === 'POST' && is_array($requestBody) ) {
             $extraHeaders['Content-type'] = 'Content-type: application/x-www-form-urlencoded';
         }
 

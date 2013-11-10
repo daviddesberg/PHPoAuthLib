92a8f40b9f6969665b6e403bc06186557c887fae
PeeHaa <info@pieterhordijk.com>
2013-11-10 16:06:34 +0100

    Added missing custom scope

--- tests/Mocks/OAuth2/Service/Fake.php
+++ tests/Mocks/OAuth2/Service/Fake.php
@@ -6,7 +6,8 @@ use OAuth\OAuth2\Service\AbstractService;
 
 class Fake extends AbstractService
 {
-    const SCOPE_FOO = 'https://www.pieterhordijk.com/auth';
+    const SCOPE_FOO    = 'https://www.pieterhordijk.com/auth';
+    const SCOPE_CUSTOM = 'custom';
 
     /**
      * {@inheritdoc}

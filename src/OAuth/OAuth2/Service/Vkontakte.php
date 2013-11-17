3dacce98ca2b1500bac85ed7784a9df2dfe7a92c
PeeHaa <info@pieterhordijk.com>
2013-11-17 17:45:48 +0100

    Added vkontakte scopes

--- src/OAuth/OAuth2/Service/Vkontakte.php
+++ src/OAuth/OAuth2/Service/Vkontakte.php
@@ -12,6 +12,32 @@ use OAuth\Common\Http\Uri\UriInterface;
 
 class Vkontakte extends AbstractService
 {
+    /**
+     * Defined scopes
+     *
+     * @link http://vk.com/dev/permissions
+     */
+    const SCOPE_NOTIFY        = 'notify';
+    const SCOPE_FRIENDS       = 'friends';
+    const SCOPE_PHOTOS        = 'photos';
+    const SCOPE_AUDIO         = 'audio';
+    const SCOPE_VIDEO         = 'video';
+    const SCOPE_DOCS          = 'docs';
+    const SCOPE_NOTES         = 'notes';
+    const SCOPE_PAGES         = 'pages';
+    const SCOPE_APP_LINK      = '';
+    const SCOPE_STATUS        = 'status';
+    const SCOPE_OFFERS        = 'offers';
+    const SCOPE_QUESTIONS     = 'questions';
+    const SCOPE_WALL          = 'wall';
+    const SCOPE_GROUPS        = 'groups';
+    const SCOPE_MESSAGES      = 'messages';
+    const SCOPE_NOTIFICATIONS = 'notifications';
+    const SCOPE_STATS         = 'stats';
+    const SCOPE_ADS           = 'ads';
+    const SCOPE_OFFLINE       = 'offline';
+    const SCOPE_NOHTTPS       = 'nohttps';
+
     public function __construct(
         CredentialsInterface $credentials,
         ClientInterface $httpClient,

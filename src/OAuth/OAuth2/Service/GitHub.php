e61a8328b21157a3ff178c1039552df369166d3d
cmfcmf <cmfcmf@users.noreply.github.com>
2013-11-24 11:50:14 +0100

    Added a full list of all scopes.
    See http://developer.github.com/v3/oauth/

--- src/OAuth/OAuth2/Service/GitHub.php
+++ src/OAuth/OAuth2/Service/GitHub.php
@@ -13,12 +13,63 @@ use OAuth\Common\Http\Uri\UriInterface;
 class GitHub extends AbstractService
 {
     /**
-     * Defined scopes, see http://developer.github.com/v3/oauth/ for definitions
+     * Defined scopes, see http://developer.github.com/v3/oauth/ for definitions.
+     */
+
+    /**
+     * Public read-only access (includes public user profile info, public repo info, and gists)
+     */
+    const SCOPE_READONLY = '';
+
+    /**
+     * Read/write access to profile info only.
+     *
+     * Includes SCOPE_USER_EMAIL and SCOPE_USER_FOLLOW.
      */
     const SCOPE_USER = 'user';
+
+    /**
+     * Read access to a user’s email addresses.
+     */
+    const SCOPE_USER_EMAIL = 'user:email';
+
+    /**
+     * Access to follow or unfollow other users.
+     */
+    const SCOPE_USER_FOLLOW = 'user:follow';
+
+    /**
+     * Read/write access to public repos and organizations.
+     */
     const SCOPE_PUBLIC_REPO = 'public_repo';
+
+    /**
+     * Read/write access to public and private repos and organizations.
+     *
+     * Includes SCOPE_REPO_STATUS.
+     */
     const SCOPE_REPO = 'repo';
+
+    /**
+     * Read/write access to public and private repository commit statuses. This scope is only necessary to grant other
+     * users or services access to private repository commit statuses without granting access to the code. The repo and
+     * public_repo scopes already include access to commit status for private and public repositories, respectively.
+     */
+    const SCOPE_REPO_STATUS = 'repo:status';
+
+    /**
+     * Delete access to adminable repositories.
+     */
     const SCOPE_DELETE_REPO = 'delete_repo';
+
+    /**
+     * Read access to a user’s notifications. repo is accepted too.
+     */
+    const SCOPE_NOTIFICATIONS = 'notifications';
+
+    /**
+     * Write access to gists.
+     */
     const SCOPE_GIST = 'gist';
 
     public function __construct(

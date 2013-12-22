5c625e660d00f5d5d10e0aec519e7fec7add5ab9
Connor Hindley <conn.hindley@gmail.com>
2013-12-21 20:34:25 -0600

    fix reddit tests

--- tests/Unit/OAuth2/Service/RedditTest.php
+++ tests/Unit/OAuth2/Service/RedditTest.php
@@ -94,7 +94,7 @@ class RedditTest extends \PHPUnit_Framework_TestCase
     public function testGetAuthorizationMethod()
     {
         $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
-        $client->expects($this->once())->method('retrieveResponse')->will($this->returnArgument(0));
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnArgument(2));
 
         $token = $this->getMock('\\OAuth\\OAuth2\\Token\\TokenInterface');
         $token->expects($this->once())->method('getEndOfLife')->will($this->returnValue(TokenInterface::EOL_NEVER_EXPIRES));
@@ -109,10 +109,10 @@ class RedditTest extends \PHPUnit_Framework_TestCase
             $storage
         );
 
-        $uri         = $service->request('https://pieterhordijk.com/my/awesome/path');
-        $absoluteUri = parse_url($uri->getAbsoluteUri());
+        $headers = $service->request('https://pieterhordijk.com/my/awesome/path');
 
-        $this->assertSame('access_token=foo', $absoluteUri['query']);
+        $this->assertTrue(array_key_exists('Authorization', $headers));
+        $this->assertTrue(in_array('Bearer foo', $headers, true));
     }
 
     /**

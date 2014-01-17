9c0feee39d05658655b5d23b9b198d92a0d3fc4e
hannesvdvreken <vandevreken.hannes@gmail.com>
2014-01-17 14:10:53 +0100

    Reorganised Google scopes and added some

--- src/OAuth/OAuth2/Service/Google.php
+++ src/OAuth/OAuth2/Service/Google.php
@@ -9,48 +9,84 @@ use OAuth\Common\Http\Uri\Uri;
 class Google extends AbstractService
 {
     /**
-     * Defined scopes -- Google has way too many Application Programming Interfaces
+     * Defined scopes - More scopes are listed here:
+     * https://developers.google.com/oauthplayground/
+     *
+     * Make a pull request if you need more scopes.
      */
-    const SCOPE_ADSENSE = 'https://www.googleapis.com/auth/adsense';
-    const SCOPE_ADWORDS = 'https://adwords.google.com/api/adwords/';
-    const SCOPE_GAN = 'https://www.googleapis.com/auth/gan'; // google affiliate network...?
-    const SCOPE_ANALYTICS = 'https://www.googleapis.com/auth/analytics.readonly';
-    const SCOPE_BOOKS = 'https://www.googleapis.com/auth/books';
-    const SCOPE_BLOGGER = 'https://www.googleapis.com/auth/blogger';
-    const SCOPE_CALENDAR = 'https://www.googleapis.com/auth/calendar';
-    const SCOPE_CLOUDSTORAGE = 'https://www.googleapis.com/auth/devstorage.read_write';
-    const SCOPE_CONTACT = 'https://www.google.com/m8/feeds/';
-    const SCOPE_CONTENTFORSHOPPING = 'https://www.googleapis.com/auth/structuredcontent'; // what even is this
-    const SCOPE_CHROMEWEBSTORE = 'https://www.googleapis.com/auth/chromewebstore.readonly';
-    const SCOPE_DOCUMENTSLIST = 'https://docs.google.com/feeds/';
-    const SCOPE_EMAIL = 'email';
-    const SCOPE_GOOGLEDRIVE = 'https://www.googleapis.com/auth/drive';
-    const SCOPE_GOOGLEDRIVE_FILES = 'https://www.googleapis.com/auth/drive.file';
-    const SCOPE_GMAIL = 'https://mail.google.com/mail/feed/atom';
-    const SCOPE_GPLUS_ME = 'https://www.googleapis.com/auth/plus.me';
-    const SCOPE_GPLUS_LOGIN = 'https://www.googleapis.com/auth/plus.login';
-    const SCOPE_GROUPS_PROVISIONING = 'https://apps-apis.google.com/a/feeds/groups/';
+    
+    // Basic
+    const SCOPE_EMAIL                       = 'email';
+    const SCOPE_PROFILE                     = 'profile';
+
+    const SCOPE_USERINFO_EMAIL              = 'https://www.googleapis.com/auth/userinfo.email';
+    const SCOPE_USERINFO_PROFILE            = 'https://www.googleapis.com/auth/userinfo.profile';
+
+    // Google+
+    const SCOPE_GPLUS_ME                    = 'https://www.googleapis.com/auth/plus.me';
+    const SCOPE_GPLUS_LOGIN                 = 'https://www.googleapis.com/auth/plus.login';
+
+    // Google Drive
+    const SCOPE_DOCUMENTSLIST               = 'https://docs.google.com/feeds/';
+    const SCOPE_SPREADSHEETS                = 'https://spreadsheets.google.com/feeds/';
+    const SCOPE_GOOGLEDRIVE                 = 'https://www.googleapis.com/auth/drive';
+    const SCOPE_DRIVE_APPS                  = 'https://www.googleapis.com/auth/drive.appdata';
+    const SCOPE_DRIVE_APPS_READ_ONLY        = 'https://www.googleapis.com/auth/drive.apps.readonly';
+    const SCOPE_GOOGLEDRIVE_FILES           = 'https://www.googleapis.com/auth/drive.file';
+    const SCOPE_DRIVE_METADATA_READ_ONLY    = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+    const SCOPE_DRIVE_READ_ONLY             = 'https://www.googleapis.com/auth/drive.readonly';
+    const SCOPE_DRIVE_SCRIPTS               = 'https://www.googleapis.com/auth/drive.scripts';
+
+    // Adwords
+    const SCOPE_ADSENSE                     = 'https://www.googleapis.com/auth/adsense';
+    const SCOPE_ADWORDS                     = 'https://adwords.google.com/api/adwords/';
+    const SCOPE_GAN                         = 'https://www.googleapis.com/auth/gan'; // google affiliate network...?
+
+    // Google Analytics
+    const SCOPE_ANALYTICS                   = 'https://www.googleapis.com/auth/analytics';
+    const SCOPE_ANALYTICS_EDIT              = 'https://www.googleapis.com/auth/analytics.edit';
+    const SCOPE_ANALYTICS_MANAGE_USERS      = 'https://www.googleapis.com/auth/analytics.manage.users';
+    const SCOPE_ANALYTICS_READ_ONLY         = 'https://www.googleapis.com/auth/analytics.readonly';
+
+    // Other services
+    const SCOPE_BOOKS                       = 'https://www.googleapis.com/auth/books';
+    const SCOPE_BLOGGER                     = 'https://www.googleapis.com/auth/blogger';
+    const SCOPE_CALENDAR                    = 'https://www.googleapis.com/auth/calendar';
+    const SCOPE_CONTACT                     = 'https://www.google.com/m8/feeds/';
+    const SCOPE_CHROMEWEBSTORE              = 'https://www.googleapis.com/auth/chromewebstore.readonly';
+    const SCOPE_GMAIL                       = 'https://mail.google.com/mail/feed/atom';
+    const SCOPE_PICASAWEB                   = 'https://picasaweb.google.com/data/';
+    const SCOPE_SITES                       = 'https://sites.google.com/feeds/';
+    const SCOPE_URLSHORTENER                = 'https://www.googleapis.com/auth/urlshortener';
+    const SCOPE_WEBMASTERTOOLS              = 'https://www.google.com/webmasters/tools/feeds/';
+    const SCOPE_TASKS                       = 'https://www.googleapis.com/auth/tasks';
+    
+    // Cloud services
+    const SCOPE_CLOUDSTORAGE                = 'https://www.googleapis.com/auth/devstorage.read_write';
+    const SCOPE_CONTENTFORSHOPPING          = 'https://www.googleapis.com/auth/structuredcontent'; // what even is this
+    const SCOPE_USER_PROVISIONING           = 'https://apps-apis.google.com/a/feeds/user/';
+    const SCOPE_GROUPS_PROVISIONING         = 'https://apps-apis.google.com/a/feeds/groups/';
+    const SCOPE_NICKNAME_PROVISIONING       = 'https://apps-apis.google.com/a/feeds/alias/';
+
+    // Old
+    const SCOPE_ORKUT                       = 'https://www.googleapis.com/auth/orkut';
     const SCOPE_GOOGLELATITUDE =
         'https://www.googleapis.com/auth/latitude.all.best https://www.googleapis.com/auth/latitude.all.city';
-        // creepy stalker api...
-    const SCOPE_MODERATOR = 'https://www.googleapis.com/auth/moderator';
-    const SCOPE_NICKNAME_PROVISIONING = 'https://apps-apis.google.com/a/feeds/alias/';
-    const SCOPE_OPENID = 'openid';
-    const SCOPE_ORKUT = 'https://www.googleapis.com/auth/orkut'; // evidently orkut still exists. who knew?
-    const SCOPE_PROFILE = 'profile';
-    const SCOPE_PICASAWEB = 'https://picasaweb.google.com/data/';
-    const SCOPE_SITES = 'https://sites.google.com/feeds/';
-    const SCOPE_SPREADSHEETS = 'https://spreadsheets.google.com/feeds/';
-    const SCOPE_TASKS = 'https://www.googleapis.com/auth/tasks';
-    const SCOPE_URLSHORTENER = 'https://www.googleapis.com/auth/urlshortener';
-    const SCOPE_USERINFO_EMAIL = 'https://www.googleapis.com/auth/userinfo.email';
-    const SCOPE_USERINFO_PROFILE = 'https://www.googleapis.com/auth/userinfo.profile';
-    const SCOPE_USER_PROVISIONING = 'https://apps-apis.google.com/a/feeds/user/';
-    const SCOPE_WEBMASTERTOOLS = 'https://www.google.com/webmasters/tools/feeds/';
-    const SCOPE_YOUTUBE = 'https://gdata.youtube.com';
+    const SCOPE_OPENID                      = 'openid';
 
-    const SCOPE_GLASS_TIMELINE = 'https://www.googleapis.com/auth/glass.timeline';
-    const SCOPE_GLASS_LOCATION = 'https://www.googleapis.com/auth/glass.location';
+    // YouTube
+    const SCOPE_YOUTUBE_GDATA               = 'https://gdata.youtube.com';
+    const SCOPE_YOUTUBE_ANALYTICS_MONETARY  = 'https://www.googleapis.com/auth/yt-analytics-monetary.readonly';
+    const SCOPE_YOUTUBE_ANALYTICS           = 'https://www.googleapis.com/auth/yt-analytics.readonly';
+    const SCOPE_YOUTUBE                     = 'https://www.googleapis.com/auth/youtube';
+    const SCOPE_YOUTUBE_READ_ONLY           = 'https://www.googleapis.com/auth/youtube.readonly';
+    const SCOPE_YOUTUBE_UPLOAD              = 'https://www.googleapis.com/auth/youtube.upload';
+    const SCOPE_YOUTUBE_PATNER              = 'https://www.googleapis.com/auth/youtubepartner';
+    const SCOPE_YOUTUBE_PARTNER_EDIT        = 'https://www.googleapis.com/auth/youtubepartner-channel-edit';
+
+    // Google Glass
+    const SCOPE_GLASS_TIMELINE              = 'https://www.googleapis.com/auth/glass.timeline';
+    const SCOPE_GLASS_LOCATION              = 'https://www.googleapis.com/auth/glass.location';
 
 
 

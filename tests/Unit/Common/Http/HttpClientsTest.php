90cf4849c3db57f3dc6ff01e70ec6503f97fd771
Raúl Pedro Santos <borfast@gmail.com>
2014-04-22 14:05:06 +0100

    Fix incorrect header name
    Should be "Content-Type" instead of "Content-type".

--- tests/Unit/Common/Http/HttpClientsTest.php
+++ tests/Unit/Common/Http/HttpClientsTest.php
@@ -1,171 +1,142 @@
 <?php
 
-/**
- * @category   OAuth
- * @package    Tests
- * @author     David Desberg <david@daviddesberg.com>
- * @copyright  Copyright (c) 2012 The authors
- * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
- */
+namespace OAuth\Common\Http\Client;
 
-namespace OAuth\Unit\Common\Http;
-
-use OAuth\Common\Http\Uri\Uri;
+use OAuth\Common\Http\Exception\TokenResponseException;
 use OAuth\Common\Http\Uri\UriInterface;
-use OAuth\Common\Http\Client;
 
-class HttpClientsTest extends \PHPUnit_Framework_TestCase
+/**
+ * Client implementation for cURL
+ */
+class CurlClient extends AbstractClient
 {
     /**
-     * @var object|\OAuth\Common\Http\Client\ClientInterface[]
-     */
-    protected $clients;
-
-    public function setUp()
-    {
-        $streamClient = new Client\StreamClient();
-        $streamClient->setTimeout(3);
-
-        $curlClient = new Client\CurlClient();
-        $curlClient->setTimeout(3);
-
-        $this->clients[] = $streamClient;
-        $this->clients[] = $curlClient;
-    }
-
-    public function tearDown()
-    {
-        foreach ($this->clients as $client) {
-            unset($client);
-        }
-    }
-
-    /**
-     * Test that extra headers are passed properly
-     */
-    public function testHeaders()
-    {
-        $testUri = new Uri('http://httpbin.org/get');
-
-        $me = $this;
-        $headerCb = function ($response) use ($me) {
-            $data = json_decode($response, true);
-            $me->assertEquals('extraheadertest', $data['headers']['Testingheader']);
-        };
-
-        $this->__doTestRetrieveResponse($testUri, array(), array('Testingheader' => 'extraheadertest'), 'GET', $headerCb);
-    }
-
-    /**
-     * Tests that we get an exception for a >= 400 status code
-     */
-    public function testException()
-    {
-        // sending a post here should get us a 405 which should trigger an exception
-        $testUri = new Uri('http://httpbin.org/delete');
-        foreach ($this->clients as $client) {
-            $this->setExpectedException('OAuth\Common\Http\Exception\TokenResponseException');
-            $client->retrieveResponse($testUri, array('blah' => 'blih'));
-        }
-    }
-
-    /**
-     * Tests the DELETE method
-     */
-    public function testDelete()
-    {
-        $testUri = new Uri('http://httpbin.org/delete');
-
-        $me = $this;
-        $deleteTestCb = function ($response) use ($me) {
-            $data = json_decode($response, true);
-            $me->assertEquals('', $data['data']);
-        };
-
-        $this->__doTestRetrieveResponse($testUri, array(), array(), 'DELETE', $deleteTestCb);
-    }
-
-    /**
-     * Tests the PUT method
-     */
-    public function testPut()
-    {
-        $testUri = new Uri('http://httpbin.org/put');
-
-        $me = $this;
-        $putTestCb = function ($response) use ($me) {
-            // verify the put response
-            $data = json_decode($response, true);
-            $me->assertEquals(json_encode(array('testKey' => 'testValue')), $data['data']);
-        };
-
-        $this->__doTestRetrieveResponse($testUri, json_encode(array('testKey' => 'testValue')), array('Content-type' => 'application/json'), 'PUT', $putTestCb);
-    }
-
-    /**
-     * Tests the POST method
-     */
-    public function testPost()
-    {
-        // http test server
-        $testUri = new Uri('http://httpbin.org/post');
-
-        $me = $this;
-        $postTestCb = function ($response) use ($me) {
-            // verify the post response
-            $data = json_decode($response, true);
-            // note that we check this because the retrieveResponse wrapper function automatically adds a content-type
-            // if there isn't one and it
-            $me->assertEquals('testValue', $data['form']['testKey']);
-        };
-
-        $this->__doTestRetrieveResponse($testUri, array('testKey' => 'testValue'), array(), 'POST', $postTestCb);
-    }
-
-    /**
-     * Expect exception when we try to send a GET request with a body
-     */
-    public function testInvalidGet()
-    {
-        $testUri =  new Uri('http://site.net');
-
-        foreach ($this->clients as $client) {
-            $this->setExpectedException('InvalidArgumentException');
-            $client->retrieveResponse($testUri, array('blah' => 'blih'), array(), 'GET');
-        }
-    }
-
-    /**
-     * Tests the GET method
-     */
-    public function testGet()
-    {
-        // test uri
-        $testUri = new Uri('http://httpbin.org/get?testKey=testValue');
-
-        $me = $this;
-        $getTestCb = function ($response) use ($me) {
-            $data = json_decode($response, true);
-            $me->assertEquals('testValue', $data['args']['testKey']);
-        };
-
-        $this->__doTestRetrieveResponse($testUri, array(), array(), 'GET', $getTestCb);
-    }
-
-    /**
-     * Test on all HTTP clients.
+     * If true, explicitly sets cURL to use SSL version 3. Use this if cURL
+     * compiles with GnuTLS SSL.
      *
-     * @param UriInterface $uri
-     * @param array        $param
-     * @param array        $header
-     * @param string       $method
-     * @param \Closure     $responseCallback
+     * @var bool
      */
-    protected function __doTestRetrieveResponse(UriInterface $uri, $param, array $header, $method, $responseCallback)
+    private $forceSSL3 = false;
+
+    /**
+     * Additional parameters (as `key => value` pairs) to be passed to `curl_setopt`
+     *
+     * @var array
+     */
+    private $parameters = array();
+
+    /**
+     * Additional `curl_setopt` parameters
+     *
+     * @param array $parameters
+     */
+    public function setCurlParameters(array $parameters)
     {
-        foreach ($this->clients as $client) {
-            $response = $client->retrieveResponse($uri, $param, $header, $method);
-            $responseCallback($response, $client);
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * @param bool $force
+     *
+     * @return CurlClient
+     */
+    public function setForceSSL3($force)
+    {
+        $this->forceSSL3 = $force;
+
+        return $this;
+    }
+
+    /**
+     * Any implementing HTTP providers should send a request to the provided endpoint with the parameters.
+     * They should return, in string form, the response body and throw an exception on error.
+     *
+     * @param UriInterface $endpoint
+     * @param mixed        $requestBody
+     * @param array        $extraHeaders
+     * @param string       $method
+     *
+     * @return string
+     *
+     * @throws TokenResponseException
+     * @throws \InvalidArgumentException
+     */
+    public function retrieveResponse(
+        UriInterface $endpoint,
+        $requestBody,
+        array $extraHeaders = array(),
+        $method = 'POST'
+    ) {
+        // Normalize method name
+        $method = strtoupper($method);
+
+        $this->normalizeHeaders($extraHeaders);
+
+        if ($method === 'GET' && !empty($requestBody)) {
+            throw new \InvalidArgumentException('No body expected for "GET" request.');
         }
+
+        if (!isset($extraHeaders['Content-Type']) && $method === 'POST' && is_array($requestBody)) {
+            $extraHeaders['Content-Type'] = 'Content-Type: application/x-www-form-urlencoded';
+        }
+
+        $extraHeaders['Host']       = 'Host: '.$endpoint->getHost();
+        $extraHeaders['Connection'] = 'Connection: close';
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $endpoint->getAbsoluteUri());
+
+        if ($method === 'POST' || $method === 'PUT') {
+            if ($requestBody && is_array($requestBody)) {
+                $requestBody = http_build_query($requestBody, '', '&');
+            }
+
+            if ($method === 'PUT') {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+            } else {
+                curl_setopt($ch, CURLOPT_POST, true);
+            }
+
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $requestBody);
+        } else {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        }
+
+        if ($this->maxRedirects > 0) {
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_MAXREDIRS, $this->maxRedirects);
+        }
+
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $extraHeaders);
+        curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
+
+        foreach ($this->parameters as $key => $value) {
+            curl_setopt($ch, $key, $value);
+        }
+
+        if ($this->forceSSL3) {
+            curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+        }
+
+        $response     = curl_exec($ch);
+        $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if (false === $response) {
+            $errNo  = curl_errno($ch);
+            $errStr = curl_error($ch);
+            curl_close($ch);
+            if (empty($errStr)) {
+                throw new TokenResponseException('Failed to request resource.', $responseCode);
+            }
+            throw new TokenResponseException('cURL Error # '.$errNo.': '.$errStr, $responseCode);
+        }
+
+        curl_close($ch);
+
+        return $response;
     }
 }

ff6cb33c9508bb6845c690096baac151338c4e58
David Desberg <Lusitanian@users.noreply.github.com>
2014-04-24 13:02:25 -0400

    Fix CS

--- src/OAuth/OAuth2/Service/Exception/InvalidAccessTypeException.php
+++ src/OAuth/OAuth2/Service/Exception/InvalidAccessTypeException.php
@@ -5,9 +5,8 @@ namespace OAuth\OAuth2\Service\Exception;
 use OAuth\Common\Exception\Exception;
 
 /**
- * Exception thrown when an invalid access_type is specified
+ * Exception thrown when an invalid accessType for the Google Service is specified
  */
 class InvalidAccessTypeException extends Exception
 {
 }
-

de897d3b550608bba166e21f5c45f2356a97023d
Zack Katz <zack@katzwebservices.com>
2014-05-05 11:59:31 -0600

    Add WordPress token storage
    Use the WordPress settings table to store tokens.

---
+++ src/OAuth/Common/Storage/WordPressMemory.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace OAuth\Common\Storage;
+
+use OAuth\Common\Token\TokenInterface;
+use OAuth\Common\Storage\Exception\TokenNotFoundException;
+
+/*
+ * Stores a token in WordPress options database table
+ */
+class WordPressMemory implements TokenStorageInterface
+{
+    /**
+     * @var object|TokenInterface
+     */
+    protected $tokens;
+
+    public function __construct()
+    {
+        $this->tokens = (array)maybe_unserialize( get_option( 'lusitanian_oauth_token' ) );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function retrieveAccessToken($service)
+    {
+        if ($this->hasAccessToken($service)) {
+            return $this->tokens[$service];
+        }
+
+        throw new TokenNotFoundException('Token not stored');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function storeAccessToken($service, TokenInterface $token)
+    {
+
+        $this->tokens[$service] = $token;
+
+        update_option( 'lusitanian_oauth_token', $this->tokens);
+
+        // allow chaining
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function hasAccessToken($service)
+    {
+        return is_array($tokens) && isset($this->tokens[$service]) && $this->tokens[$service] instanceof TokenInterface;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function clearToken($service)
+    {
+        if (array_key_exists($service, $this->tokens)) {
+            unset($this->tokens[$service]);
+        }
+
+        update_option( 'lusitanian_oauth_token', $this->tokens);
+
+        // allow chaining
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function clearAllTokens()
+    {
+
+        $this->tokens = array();
+
+        update_option( 'lusitanian_oauth_token', $this->tokens);
+
+        // allow chaining
+        return $this;
+    }
+}

3e2ea00e29af64c7a20c41ee37a21e61c1a05766
Andrii Gakhov <gakhov@ferret-go.com>
2014-09-26 14:22:46 +0200

    defined allowed scopes

--- src/OAuth/OAuth2/Service/JawboneUP.php
+++ src/OAuth/OAuth2/Service/JawboneUP.php
@@ -18,6 +18,37 @@ use OAuth\Common\Http\Uri\UriInterface;
  */
 class JawboneUP extends AbstractService
 {
+    /**
+     * Defined scopes
+     *
+     *
+     * @link https://jawbone.com/up/developer/authentication
+     */
+    // general information scopes
+    const SCOPE_BASIC_READ          = 'basic_read';
+    const SCOPE_EXTENDED_READ       = 'extended_read';
+    const SCOPE_LOCATION_READ       = 'location_read';
+    const SCOPE_FRIENDS_READ        = 'friends_read';
+    // mood scopes
+    const SCOPE_MOOD_READ           = 'mood_read';
+    const SCOPE_MOOD_WRITE          = 'mood_write';
+    // move scopes
+    const SCOPE_MOVE_READ           = 'move_read';
+    const SCOPE_MOVE_WRITE          = 'move_write';
+    // sleep scopes
+    const SCOPE_SLEEP_READ          = 'sleep_read';
+    const SCOPE_SLEEP_WRITE         = 'sleep_write';
+    // meal scopes
+    const SCOPE_MEAL_READ           = 'meal_read';
+    const SCOPE_MEAL_WRITE          = 'meal_write';
+    // weight scopes
+    const SCOPE_WEIGHT_READ         = 'weight_read';
+    const SCOPE_WEIGHT_WRITE        = 'weight_write';
+    // generic event scopes
+    const SCOPE_GENERIC_EVENT_READ  = 'generic_event_read';
+    const SCOPE_GENERIC_EVENT_WRITE = 'generic_event_write';
+
+
     public function __construct(
         CredentialsInterface $credentials,
         ClientInterface $httpClient,

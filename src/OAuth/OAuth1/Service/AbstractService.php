daviddesberg/PHPoAuthLib

commit c6e2d74eca2b8bc6f9d9aea183655886e49b5216

Pull body params into authorization header.

src/OAuth/OAuth1/Service/AbstractService.php
@@ -203,10 +203,8 @@ abstract class AbstractService extends BaseAbstractService implements ServiceInt
         }
 
         $parameters = array_merge($parameters, array('oauth_token' => $token->getAccessToken()));
-
-        $mergedParams = (is_array($bodyParams)) ? array_merge($parameters, $bodyParams) : $parameters;
-
-        $parameters['oauth_signature'] = $this->signature->getSignature($uri, $mergedParams, $method);
+        $parameters = (is_array($bodyParams)) ? array_merge($parameters, $bodyParams) : $parameters;
+        $parameters['oauth_signature'] = $this->signature->getSignature($uri, $parameters, $method);
 
         $authorizationHeader = 'OAuth ';
         $delimiter = '';

commit 07a49242690a09e4dd9811407bf8d5039653bda8

fixed bug introduced by commit c6e2d74eca2b8bc6f9d9aea183655886e49b5216

src/OAuth/OAuth1/Service/AbstractService.php
@@ -197,19 +197,20 @@ abstract class AbstractService extends BaseAbstractService implements ServiceInt
         $bodyParams = null
     ) {
         $this->signature->setTokenSecret($token->getAccessTokenSecret());
-        $parameters = $this->getBasicAuthorizationHeaderInfo();
-        if (isset($parameters['oauth_callback'])) {
-            unset($parameters['oauth_callback']);
+        $authParameters = $this->getBasicAuthorizationHeaderInfo();
+        if (isset($authParameters['oauth_callback'])) {
+            unset($authParameters['oauth_callback']);
         }
 
-        $parameters = array_merge($parameters, array('oauth_token' => $token->getAccessToken()));
-        $parameters = (is_array($bodyParams)) ? array_merge($parameters, $bodyParams) : $parameters;
-        $parameters['oauth_signature'] = $this->signature->getSignature($uri, $parameters, $method);
+        $authParameters = array_merge($authParameters, array('oauth_token' => $token->getAccessToken()));
+
+        $signatureParams = (is_array($bodyParams)) ? array_merge($authParameters, $bodyParams) : $authParameters;
+        $authParameters['oauth_signature'] = $this->signature->getSignature($uri, $signatureParams, $method);
 
         $authorizationHeader = 'OAuth ';
         $delimiter = '';
 
-        foreach ($parameters as $key => $value) {
+        foreach ($authParameters as $key => $value) {
             $authorizationHeader .= $delimiter . rawurlencode($key) . '="' . rawurlencode($value) . '"';
             $delimiter = ', ';
         }
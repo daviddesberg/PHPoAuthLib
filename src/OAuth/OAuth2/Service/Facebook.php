95e745b7fb6549ea73bc4e67e607663c566d1843
Mirko Renzetti <mirko.renzetti@gmail.com>
2015-03-20 16:59:00 +0100

    Added 'user_tagged_places' Facebook scope

--- src/OAuth/OAuth2/Service/Facebook.php
+++ src/OAuth/OAuth2/Service/Facebook.php
@@ -57,6 +57,7 @@ class Facebook extends AbstractService
     // Extended Profile Properties
     const SCOPE_USER_FRIENDS                  = 'user_friends';
     const SCOPE_USER_ABOUT                    = 'user_about_me';
+    const SCOPE_USER_TAGGED_PLACES            = 'user_tagged_places';
     const SCOPE_FRIENDS_ABOUT                 = 'friends_about_me';
     const SCOPE_USER_ACTIVITIES               = 'user_activities';
     const SCOPE_FRIENDS_ACTIVITIES            = 'friends_activities';

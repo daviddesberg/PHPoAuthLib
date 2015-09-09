165db62a6a41aaa4fbad9ddb8735a481c469a97d
George <info@geodeveloper.net>
2015-09-09 10:50:01 +0300

    Indent with 4 spaces

--- src/OAuth/OAuth2/Service/Yahoo.php
+++ src/OAuth/OAuth2/Service/Yahoo.php
@@ -9,70 +9,70 @@ use OAuth\Common\Http\Uri\Uri;
 class Yahoo extends AbstractService
 {
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getAuthorizationEndpoint()
-	{
-		return new Uri('https://api.login.yahoo.com/oauth2/request_auth');
-	}
+    /**
+    * {@inheritdoc}
+    */
+    public function getAuthorizationEndpoint()
+    {
+        return new Uri('https://api.login.yahoo.com/oauth2/request_auth');
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getAccessTokenEndpoint()
-	{
-		return new Uri('https://api.login.yahoo.com/oauth2/get_token');
-	}
+    /**
+    * {@inheritdoc}
+    */
+    public function getAccessTokenEndpoint()
+    {
+        return new Uri('https://api.login.yahoo.com/oauth2/get_token');
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getAuthorizationMethod()
-	{
-		return static::AUTHORIZATION_METHOD_HEADER_BEARER;
-	}
+    /**
+    * {@inheritdoc}
+    */
+    protected function getAuthorizationMethod()
+    {
+        return static::AUTHORIZATION_METHOD_HEADER_BEARER;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function parseAccessTokenResponse($responseBody)
-	{
-		$data = json_decode($responseBody, true);
+    /**
+    * {@inheritdoc}
+    */
+    protected function parseAccessTokenResponse($responseBody)
+    {
+        $data = json_decode($responseBody, true);
 
-		if (null === $data || !is_array($data))
-		{
-			throw new TokenResponseException('Unable to parse response.');
-		} elseif (isset($data['error']))
-		{
-			throw new TokenResponseException('Error in retrieving token: "' . $data['error'] . '"');
-		}
+        if (null === $data || !is_array($data))
+        {
+            throw new TokenResponseException('Unable to parse response.');
+        } elseif (isset($data['error']))
+        {
+            throw new TokenResponseException('Error in retrieving token: "' . $data['error'] . '"');
+        }
 
-		$token = new StdOAuth2Token();
-		$token->setAccessToken($data['access_token']);
-		$token->setLifetime($data['expires_in']);
+        $token = new StdOAuth2Token();
+        $token->setAccessToken($data['access_token']);
+        $token->setLifetime($data['expires_in']);
 
-		if (isset($data['refresh_token']))
-		{
-			$token->setRefreshToken($data['refresh_token']);
-			unset($data['refresh_token']);
-		}
+        if (isset($data['refresh_token']))
+        {
+            $token->setRefreshToken($data['refresh_token']);
+            unset($data['refresh_token']);
+        }
 
-		unset($data['access_token']);
-		unset($data['expires_in']);
+        unset($data['access_token']);
+        unset($data['expires_in']);
 
-		$token->setExtraParams($data);
+        $token->setExtraParams($data);
 
-		return $token;
-	}
+        return $token;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getExtraOAuthHeaders()
-	{
-		$encodedCredentials = base64_encode($this->credentials->getConsumerId() . ':' . $this->credentials->getConsumerSecret());
-		return ['Authorization' => 'Basic ' . $encodedCredentials];
-	}
+    /**
+    * {@inheritdoc}
+    */
+    protected function getExtraOAuthHeaders()
+    {
+        $encodedCredentials = base64_encode($this->credentials->getConsumerId() . ':' . $this->credentials->getConsumerSecret());
+        return ['Authorization' => 'Basic ' . $encodedCredentials];
+    }
 
 }

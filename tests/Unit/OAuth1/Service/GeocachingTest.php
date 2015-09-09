c591745c271c811df11368fd9935a3ebec3d0657
Surfoo <surfooo@gmail.com>
2015-09-09 14:50:09 +0200

    Geocaching OAuth1 Service

---
+++ tests/Unit/OAuth1/Service/GeocachingTest.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace OAuthTest\Unit\OAuth1\Service;
+
+use OAuth\OAuth1\Service\Geocaching;
+
+class GeocachingTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected function getGeocachingInstance() {
+
+        return new Geocaching(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface'),
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     */
+    public function testConstructCorrectInterfaceWithoutCustomUri()
+    {
+        $service = $this->getGeocachingInstance();
+
+        $this->assertInstanceOf('\\OAuth\\OAuth1\\Service\\ServiceInterface', $service);
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     */
+    public function testConstructCorrectInstanceWithoutCustomUri()
+    {
+        $service = $this->getGeocachingInstance();
+
+        $this->assertInstanceOf('\\OAuth\\OAuth1\\Service\\AbstractService', $service);
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     */
+    public function testConstructCorrectInstanceWithCustomUri()
+    {
+        $service = $this->getGeocachingInstance();
+
+        $this->assertInstanceOf('\\OAuth\\OAuth1\\Service\\AbstractService', $service);
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     * @covers OAuth\OAuth1\Service\Geocaching::getRequestTokenEndpoint
+     */
+    public function testGetRequestTokenEndpoint()
+    {
+        $service = $this->getGeocachingInstance();
+
+        $this->assertSame(
+            'https://www.geocaching.com/OAuth/oauth.ashx',
+            $service->getRequestTokenEndpoint()->getAbsoluteUri()
+        );
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     * @covers OAuth\OAuth1\Service\Geocaching::getAuthorizationEndpoint
+     */
+    public function testGetAuthorizationEndpoint()
+    {
+        $service = $this->getGeocachingInstance();
+
+        $this->assertSame(
+            'https://www.geocaching.com/OAuth/oauth.ashx',
+            $service->getAuthorizationEndpoint()->getAbsoluteUri()
+        );
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     * @covers OAuth\OAuth1\Service\Geocaching::getAccessTokenEndpoint
+     */
+    public function testGetAccessTokenEndpoint()
+    {
+        $service = $this->getGeocachingInstance();
+
+        $this->assertSame(
+            'https://www.geocaching.com/OAuth/oauth.ashx',
+            $service->getAccessTokenEndpoint()->getAbsoluteUri()
+        );
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     * @covers OAuth\OAuth1\Service\Geocaching::getRequestTokenEndpoint
+     * @covers OAuth\OAuth1\Service\Geocaching::parseRequestTokenResponse
+     */
+    public function testParseRequestTokenResponseThrowsExceptionOnNulledResponse()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue(null));
+
+        $service = new Geocaching(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestRequestToken();
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     * @covers OAuth\OAuth1\Service\Geocaching::getRequestTokenEndpoint
+     * @covers OAuth\OAuth1\Service\Geocaching::parseRequestTokenResponse
+     */
+    public function testParseRequestTokenResponseThrowsExceptionOnResponseNotAnArray()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('notanarray'));
+
+        $service = new Geocaching(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestRequestToken();
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     * @covers OAuth\OAuth1\Service\Geocaching::getRequestTokenEndpoint
+     * @covers OAuth\OAuth1\Service\Geocaching::parseRequestTokenResponse
+     */
+    public function testParseRequestTokenResponseThrowsExceptionOnResponseCallbackNotSet()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('foo=bar'));
+
+        $service = new Geocaching(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestRequestToken();
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     * @covers OAuth\OAuth1\Service\Geocaching::getRequestTokenEndpoint
+     * @covers OAuth\OAuth1\Service\Geocaching::parseRequestTokenResponse
+     */
+    public function testParseRequestTokenResponseThrowsExceptionOnResponseCallbackNotTrue()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue(
+            'oauth_callback_confirmed=false'
+        ));
+
+        $service = new Geocaching(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestRequestToken();
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     * @covers OAuth\OAuth1\Service\Geocaching::getRequestTokenEndpoint
+     * @covers OAuth\OAuth1\Service\Geocaching::parseRequestTokenResponse
+     * @covers OAuth\OAuth1\Service\Geocaching::parseAccessTokenResponse
+     */
+    public function testParseRequestTokenResponseValid()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue(
+            'oauth_callback_confirmed=true&oauth_token=foo&oauth_token_secret=bar'
+        ));
+
+        $service = new Geocaching(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->assertInstanceOf('\\OAuth\\OAuth1\\Token\\StdOAuth1Token', $service->requestRequestToken());
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     * @covers OAuth\OAuth1\Service\Geocaching::getRequestTokenEndpoint
+     * @covers OAuth\OAuth1\Service\Geocaching::parseAccessTokenResponse
+     */
+    public function testParseAccessTokenResponseThrowsExceptionOnError()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('error=bar'));
+
+        $token = $this->getMock('\\OAuth\\OAuth1\\Token\\TokenInterface');
+
+        $storage = $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface');
+        $storage->expects($this->any())->method('retrieveAccessToken')->will($this->returnValue($token));
+
+        $service = new Geocaching(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $storage,
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestAccessToken('foo', 'bar', $token);
+    }
+
+    /**
+     * @covers OAuth\OAuth1\Service\Geocaching::__construct
+     * @covers OAuth\OAuth1\Service\Geocaching::getRequestTokenEndpoint
+     * @covers OAuth\OAuth1\Service\Geocaching::parseAccessTokenResponse
+     */
+    public function testParseAccessTokenResponseValid()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue(
+            'oauth_token=foo&oauth_token_secret=bar'
+        ));
+
+        $token = $this->getMock('\\OAuth\\OAuth1\\Token\\TokenInterface');
+
+        $storage = $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface');
+        $storage->expects($this->any())->method('retrieveAccessToken')->will($this->returnValue($token));
+
+        $service = new Geocaching(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $storage,
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->assertInstanceOf('\\OAuth\\OAuth1\\Token\\StdOAuth1Token', $service->requestAccessToken('foo', 'bar', $token));
+    }
+}

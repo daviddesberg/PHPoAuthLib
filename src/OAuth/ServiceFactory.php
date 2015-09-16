ffd856e42e6b147235a736191307b3cbf618e65a
leansoft <leansoft@gmail.com>
2015-09-16 22:20:05 +0200

    Autoload fix

--- src/OAuth/ServiceFactory.php
+++ src/OAuth/ServiceFactory.php
@@ -117,7 +117,7 @@ class ServiceFactory
         foreach ($this->serviceBuilders as $version => $buildMethod) {
             $fullyQualifiedServiceName = $this->getFullyQualifiedServiceName($serviceName, $version);
 
-            if (class_exists($fullyQualifiedServiceName)) {
+            if (class_exists($fullyQualifiedServiceName, false)) {
                 return $this->$buildMethod(
                     $fullyQualifiedServiceName,
                     $credentials,

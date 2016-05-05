5f78c124e1f8222f6415bc1a6636d7a5c48c5ae8
srggroup <adam.balint@srg.hu>
2016-05-05 11:16:03 +0200

    Wrike service - fixing some code style problems

--- src/OAuth/OAuth2/Service/Wrike.php
+++ src/OAuth/OAuth2/Service/Wrike.php
@@ -26,53 +26,55 @@ use OAuth\Common\Http\Uri\UriInterface;
  */
 class Wrike extends AbstractService
 {
-    /**
+    /*
      * Defined scopes
      * https://developers.wrike.com/documentation/oauth2
      */
-	const SCOPE_DEFAULT                 = 'Default';
-	const SCOPE_READ                    = 'wsReadOnly';
-	const SCOPE_READ_WRITE              = 'wsReadWrite';
-	const SCOPE_WORKFLOW_READ           = 'amReadOnlyWorkflow';
-	const SCOPE_WORKFLOW_READ_WRITE     = 'amReadWriteWorkflow';
-	const SCOPE_INVITATION_READ         = 'amReadOnlyInvitation';
-	const SCOPE_INVITATION_READ_WRITE   = 'amReadWriteInvitation';
-	const SCOPE_GROUP_READ              = 'amReadOnlyGroup';
-	const SCOPE_GROUP_READ_WRITE        = 'amReadWriteGroup';
-	const SCOPE_USER_READ               = 'amReadOnlyUser';
-	const SCOPE_USER_READ_WRITE         = 'amReadWriteUser';
+    const SCOPE_DEFAULT                 = 'Default';
+    const SCOPE_READ                    = 'wsReadOnly';
+    const SCOPE_READ_WRITE              = 'wsReadWrite';
+    const SCOPE_WORKFLOW_READ           = 'amReadOnlyWorkflow';
+    const SCOPE_WORKFLOW_READ_WRITE     = 'amReadWriteWorkflow';
+    const SCOPE_INVITATION_READ         = 'amReadOnlyInvitation';
+    const SCOPE_INVITATION_READ_WRITE   = 'amReadWriteInvitation';
+    const SCOPE_GROUP_READ              = 'amReadOnlyGroup';
+    const SCOPE_GROUP_READ_WRITE        = 'amReadWriteGroup';
+    const SCOPE_USER_READ               = 'amReadOnlyUser';
+    const SCOPE_USER_READ_WRITE         = 'amReadWriteUser';
+
+    const API_VERSION = '3';
 
 
-	public function __construct(
-		CredentialsInterface $credentials,
-		ClientInterface $httpClient,
-		TokenStorageInterface $storage,
-		$scopes = array(),
-		UriInterface $baseApiUri = null,
-		$stateParameterInAutUrl = false,
-		$apiVersion = "3" // The latest API version
-	) {
-		parent::__construct(
-			$credentials,
-			$httpClient,
-			$storage,
-			$scopes,
-			$baseApiUri,
-			$stateParameterInAutUrl,
-			$apiVersion
-		);
+    public function __construct(
+        CredentialsInterface $credentials,
+        ClientInterface $httpClient,
+        TokenStorageInterface $storage,
+        $scopes = array(),
+        UriInterface $baseApiUri = null,
+        $stateParameterInAutUrl = false,
+        $apiVersion = self::API_VERSION // The latest API version
+    ) {
+        parent::__construct(
+            $credentials,
+            $httpClient,
+            $storage,
+            $scopes,
+            $baseApiUri,
+            $stateParameterInAutUrl,
+            $apiVersion
+        );
 
-		if (null === $baseApiUri) {
-			$this->baseApiUri = new Uri('https://www.wrike.com/api'.$this->getApiVersionString().'/');
-		}
-	}
+        if (null === $baseApiUri) {
+            $this->baseApiUri = new Uri('https://www.wrike.com/api'.$this->getApiVersionString().'/');
+        }
+    }
 
     /**
      * {@inheritdoc}
      */
     public function getAuthorizationEndpoint()
     {
-		return new Uri('https://www.wrike.com/oauth2/authorize');
+        return new Uri('https://www.wrike.com/oauth2/authorize');
     }
 
     /**
@@ -80,60 +82,60 @@ class Wrike extends AbstractService
      */
     public function getAccessTokenEndpoint()
     {
-	    return new Uri('https://www.wrike.com/oauth2/token');
+        return new Uri('https://www.wrike.com/oauth2/token');
     }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function parseAccessTokenResponse($responseBody)
-	{
-		$data = json_decode($responseBody, true);
+    /**
+    * {@inheritdoc}
+    */
+    protected function parseAccessTokenResponse($responseBody)
+    {
+        $data = json_decode($responseBody, true);
 
-		if (null === $data || !is_array($data)) {
-			throw new TokenResponseException('Unable to parse response.');
-		} elseif (isset($data['error'])) {
-			throw new TokenResponseException('Error in retrieving token: "' . $data['error'] . '"');
-		}
+        if (null === $data || !is_array($data)) {
+            throw new TokenResponseException('Unable to parse response.');
+        } elseif (isset($data['error'])) {
+            throw new TokenResponseException('Error in retrieving token: "' . $data['error'] . '"');
+        }
 
-		$token = new StdOAuth2Token();
-		$token->setAccessToken($data['access_token']);
-		$token->setLifetime($data['expires_in']);
+        $token = new StdOAuth2Token();
+        $token->setAccessToken($data['access_token']);
+        $token->setLifetime($data['expires_in']);
 
-		if(isset($data['refresh_token'])){
-			$token->setRefreshToken($data['refresh_token']);
-			unset($data['refresh_token']);
-		}
+        if(isset($data['refresh_token'])){
+            $token->setRefreshToken($data['refresh_token']);
+            unset($data['refresh_token']);
+        }
 
-		unset($data['access_token']);
-		unset($data['expires_in']);
+        unset($data['access_token']);
+        unset($data['expires_in']);
 
-		$token->setExtraParams($data);
+        $token->setExtraParams($data);
 
-		return $token;
-	}
+        return $token;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getApiVersionString()
-	{
-		return empty($this->apiVersion) ? '' : '/v' . $this->apiVersion;
-	}
+    /**
+    * {@inheritdoc}
+    */
+    protected function getApiVersionString()
+    {
+        return empty($this->apiVersion) ? '' : '/v' . $this->apiVersion;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getScopesDelimiter()
-	{
-		return ',';
-	}
+    /**
+    * {@inheritdoc}
+    */
+    protected function getScopesDelimiter()
+    {
+        return ',';
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getAuthorizationMethod()
-	{
-		return static::AUTHORIZATION_METHOD_HEADER_BEARER;
-	}
+    /**
+    * {@inheritdoc}
+    */
+    protected function getAuthorizationMethod()
+    {
+        return static::AUTHORIZATION_METHOD_HEADER_BEARER;
+    }
 }
--- tests/Unit/OAuth2/Service/WrikeTest.php
+++ tests/Unit/OAuth2/Service/WrikeTest.php
@@ -87,59 +87,59 @@ class WrikeTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-	/**
-	 * @covers OAuth\OAuth2\Service\Wrike::__construct
-	 * @covers OAuth\OAuth2\Service\Wrike::parseAccessTokenResponse
-	 */
-	public function testParseAccessTokenResponseThrowsExceptionOnError()
-	{
-		$client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
-		$client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('{"error":"errorcode"}'));
+    /**
+     * @covers OAuth\OAuth2\Service\Wrike::__construct
+     * @covers OAuth\OAuth2\Service\Wrike::parseAccessTokenResponse
+     */
+    public function testParseAccessTokenResponseThrowsExceptionOnError()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('{"error":"errorcode"}'));
 
-		$service = new Wrike(
-			$this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
-			$client,
-			$this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
-		);
+        $service = new Wrike(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
+        );
 
-		$this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
 
-		$service->requestAccessToken('foo');
-	}
+        $service->requestAccessToken('foo');
+    }
 
-	/**
-	 * @covers OAuth\OAuth2\Service\Wrike::__construct
-	 * @covers OAuth\OAuth2\Service\Wrike::parseAccessTokenResponse
-	 */
-	public function testParseAccessTokenResponseValidWithoutRefreshToken()
-	{
-		$client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
-		$client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('{"access_token":"foo","expires_in":"bar"}'));
+    /**
+     * @covers OAuth\OAuth2\Service\Wrike::__construct
+     * @covers OAuth\OAuth2\Service\Wrike::parseAccessTokenResponse
+     */
+    public function testParseAccessTokenResponseValidWithoutRefreshToken()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('{"access_token":"foo","expires_in":"bar"}'));
 
-		$service = new Wrike(
-			$this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
-			$client,
-			$this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
-		);
+        $service = new Wrike(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
+        );
 
-		$this->assertInstanceOf('\\OAuth\\OAuth2\\Token\\StdOAuth2Token', $service->requestAccessToken('foo'));
-	}
+        $this->assertInstanceOf('\\OAuth\\OAuth2\\Token\\StdOAuth2Token', $service->requestAccessToken('foo'));
+    }
 
-	/**
-	 * @covers OAuth\OAuth2\Service\Wrike::__construct
-	 * @covers OAuth\OAuth2\Service\Wrike::parseAccessTokenResponse
-	 */
-	public function testParseAccessTokenResponseValidWithRefreshToken()
-	{
-		$client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
-		$client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('{"access_token":"foo","expires_in":"bar","refresh_token":"baz"}'));
+    /**
+     * @covers OAuth\OAuth2\Service\Wrike::__construct
+     * @covers OAuth\OAuth2\Service\Wrike::parseAccessTokenResponse
+     */
+    public function testParseAccessTokenResponseValidWithRefreshToken()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('{"access_token":"foo","expires_in":"bar","refresh_token":"baz"}'));
 
-		$service = new Wrike(
-			$this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
-			$client,
-			$this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
-		);
+        $service = new Wrike(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
+        );
 
-		$this->assertInstanceOf('\\OAuth\\OAuth2\\Token\\StdOAuth2Token', $service->requestAccessToken('foo'));
-	}
+        $this->assertInstanceOf('\\OAuth\\OAuth2\\Token\\StdOAuth2Token', $service->requestAccessToken('foo'));
+    }
 }

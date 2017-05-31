b6c180de98d3ed967af1a5552ecb52f32d2bdace
Duncan Rumbold <duncan.rumbold@dna.co.nz>
2017-05-31 13:40:37 +1200

    Update Mailchimp API version to 3.0.

--- src/OAuth/OAuth2/Service/Mailchimp.php
+++ src/OAuth/OAuth2/Service/Mailchimp.php
@@ -106,7 +106,7 @@ class Mailchimp extends AbstractService
         $meta = json_decode($response, true);
 
         // Set base api uri.
-        $this->baseApiUri = new Uri('https://'. $meta['dc'] .'.api.mailchimp.com/2.0/');
+        $this->baseApiUri = new Uri('https://'. $meta['dc'] .'.api.mailchimp.com/3.0/');
 
         // Allow chaining.
         return $this;
